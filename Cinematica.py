5baaf16381b7c78f7d2926083d21f733b7e039dd
Ricardo Machado <72943956+ricardoamachado@users.noreply.github.com>
2026-05-30 01:06:45 -0300

    Cálculo do theta_3, theta_2 e theta_4. Ainda com erro.

--- Cinematica.py
+++ Cinematica.py
@@ -48,6 +48,18 @@ def build_forward_kinematics(tabela_dh:list[DHModel]):
         matriz_transform = matriz_transform @ build_dh_matrix(linha)
     return matriz_transform
 
+def build_test(joints_params):
+    dh_table = [
+        DHModel(d=UR5Params.d[1],a=UR5Params.a[1],theta=joints_params[1], alpha=UR5Params.alpha[1]),
+        DHModel(d=UR5Params.d[2],a=UR5Params.a[2],theta=joints_params[2], alpha=UR5Params.alpha[2]),
+        DHModel(d=UR5Params.d[3],a=UR5Params.a[3],theta=joints_params[3], alpha=UR5Params.alpha[3]),
+    ]
+    model_T = np.eye(4)
+    for row in dh_table:
+        model_T = model_T @ build_dh_matrix(row)
+
+    return model_T
+
 def ur5_forward_kinematics(joints_params) -> np.ndarray:
     model_T = np.eye(4)
     dh_table = build_ur5_model(joints_params)
@@ -57,7 +69,7 @@ def ur5_forward_kinematics(joints_params) -> np.ndarray:
 
     return model_T
 
-def ur5_inverse_kinematics(desired_t_matrix:np.ndarray,shoulder="right",wrist="up"):
+def ur5_inverse_kinematics(desired_t_matrix:np.ndarray,shoulder="left",wrist="up",elbow="up"):
     assert desired_t_matrix.shape == (4,4)
     # Notação p_i_ref_j.
     # Representa a origem do frame de referência i em relação ao sistema j.
@@ -65,20 +77,19 @@ def ur5_inverse_kinematics(desired_t_matrix:np.ndarray,shoulder="right",wrist="u
     # Determinação de theta_1.
     p_5_ref_0 = desired_t_matrix @ np.array([0,0,-UR5Params.d[5],1]).T
     phi_1 = np.atan2(p_5_ref_0[1],p_5_ref_0[0])
-    phi_2 = np.acos(UR5Params.d[3]/(np.sqrt(p_5_ref_0[0]**2 + p_5_ref_0[1]**2)))
+    np.hypot(p_5_ref_0[0],p_5_ref_0[1])
+    phi_2 = np.acos(UR5Params.d[3]/(np.hypot(p_5_ref_0[0],p_5_ref_0[1])))
     if shoulder == "left":
         theta_1 = phi_1 + phi_2 + np.pi/2
     else:
         theta_1 = phi_1 - phi_2 + np.pi/2
-    print(np.rad2deg(theta_1))
 
     # Determinação de theta_5.
     p_6_ref_0 = desired_t_matrix @ np.array([0,0,0,1]).T
     theta_5 = np.acos((p_6_ref_0[0] * np.sin(theta_1) - p_6_ref_0[1] * np.cos(theta_1) - UR5Params.d[3])/UR5Params.d[5])
     if wrist == "down":
         theta_5 = - theta_5
-    print(np.rad2deg(theta_5))
-    
+
     # Determinação de theta_6.
     # Calcula os vetores x e y do sistema 0 em relação ao sistema 6.
     # Inverte a matriz T de 6 para 0.
@@ -89,8 +100,46 @@ def ur5_inverse_kinematics(desired_t_matrix:np.ndarray,shoulder="right",wrist="u
         (-x_0_ref_6[1] * np.sin(theta_1) + y_0_ref_6[1] * np.cos(theta_1))/np.sin(theta_5),
         (x_0_ref_6[0] * np.sin(theta_1) - y_0_ref_6[0] * np.cos(theta_1))/np.sin(theta_5)
         )
-    print(np.rad2deg(theta_6))
 
+    # Determinação de theta_3.
+    # Determinação das matrizes de transformação.
+    #t_m_to_n é o equivalente a T_{m}^{n}
+    t_5_to_4 = build_dh_matrix(
+        DHModel(d=UR5Params.d[4],a=UR5Params.a[4],theta=theta_5, alpha=UR5Params.alpha[4])
+        )
+    t_6_to_5 = build_dh_matrix(
+        DHModel(d=UR5Params.d[5],a=UR5Params.a[5],theta=theta_6, alpha=UR5Params.alpha[5])
+        )
+    t_1_to_0 = build_dh_matrix(
+    DHModel(d=UR5Params.d[0],a=UR5Params.a[0],theta=theta_1, alpha=UR5Params.alpha[0])
+        )
+    t_6_to_1 = (np.linalg.inv(t_1_to_0) @ desired_t_matrix)
+    t_6_to_4 = t_5_to_4 @ t_6_to_5
+    # Transforma de 4 para 6, de 6 para 0 e de 0 para 1.
+    t_4_to_1 = t_6_to_1 @ np.linalg.inv(t_6_to_4)
+    p_4_ref_1 = t_4_to_1 @ np.array([0,0,0,1]).T
+    p_4xz = np.hypot(p_4_ref_1[2],p_4_ref_1[0])
+    cos_theta_3 = (p_4xz**2 - UR5Params.a[1]**2 - UR5Params.a[2]**2)/(2 * UR5Params.a[1] * UR5Params.a[2])
+    theta_3 = np.acos(cos_theta_3)
+    if elbow == "down":
+        theta_3 = - theta_3
+
+    # Determinação de theta_2.
+    theta_2 = np.atan2(-p_4_ref_1[2],-p_4_ref_1[0]) - np.asin(-UR5Params.a[2] * np.sin(theta_3)/(p_4xz))
+
+    # Determinação de theta_4.
+    t_3_to_2 = build_dh_matrix(
+    DHModel(d=UR5Params.d[2],a=UR5Params.a[2],theta=theta_3, alpha=UR5Params.alpha[2])
+        )
+    t_2_to_1 = build_dh_matrix(
+    DHModel(d=UR5Params.d[1],a=UR5Params.a[1],theta=theta_2, alpha=UR5Params.alpha[1])
+        )
+    t_3_to_1 = t_2_to_1 @ t_3_to_2
+    t_4_to_3 = np.linalg.inv(t_3_to_1) @ t_4_to_1
+    x_4_ref_3 = t_4_to_3 @ np.array([1,0,0,0])
+    theta_4 = np.atan2(x_4_ref_3[1],x_4_ref_3[0])
+
+    return [theta_1,theta_2, theta_3, theta_4, theta_5, theta_6]
 
 def set_joints_position(joints_num:list[int],joints_params:list[float]):
     for joint, param in zip(joints_num,joints_params):
@@ -127,11 +176,13 @@ def get_Tmatrix(joints_handles:list[int],ref_handle:int):
 #sim = client.require("sim")
 
 def main_model():
-    joints_params = np.deg2rad([126,79,64,157,62,70])
+    joints_params = np.deg2rad([10, -40, 50, 20, 30, 40])
     model_T = ur5_forward_kinematics(joints_params)
     print("Model Matrix")
     print(model_T.round(5))
-    ur5_inverse_kinematics(model_T)
+    desired_q_params = ur5_inverse_kinematics(model_T)
+    print("Joints params values:")
+    print(np.rad2deg(desired_q_params))
 
 def main():
     sim.setStepping(True)
